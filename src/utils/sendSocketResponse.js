a6bbf71497653ba17446989ed12654cfc35159e7
Ivan Akulov <gxoptg@gmail.com>
2018-01-15 18:35:57 +0300

    Increase the chunk size

--- src/utils/sendSocketResponse.js
+++ src/utils/sendSocketResponse.js
@@ -2,7 +2,7 @@ const util = require('util');
 const splitToChunks = require('./splitToChunks');
 
 const sendSocketResponse = (response, socket, port, address) => {
-    const chunks = splitToChunks(response, 500);
+    const chunks = splitToChunks(response, 50000);
     const promises = chunks.map((chunk, index) => {
         const asyncSend = util.promisify(socket.send.bind(socket));
         return asyncSend(`${index}::${chunks.length}::${chunk}`, port, address);
